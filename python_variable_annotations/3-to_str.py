293a9ee41fdf1863acfc19a3f8a5fe58c5decce2
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-30 11:12:20 -0700

    function to_Str ( type-annotated )

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that contains a function that transforms
+a decimal into a string"""
+
+
+def to_str(n: float) -> str:
+    """Function that returns a string"""
+    return str(n)
