6205a6bb1d1814651a1b2db5b63f0a8fd9a5c247
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-30 11:57:23 -0700

    function sum_mix ( type-annotated )

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that contains a function"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Function that adds a list"""
+    return sum(mxd_lst)
