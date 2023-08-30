85ed56210c2331149cb4cc92c5a14b6cc961bba8
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-30 11:02:29 -0700

    function concat ( type-annotated )

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that contains a function that joins
+tow strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function that joins two strings"""
+    return str1 + str2
