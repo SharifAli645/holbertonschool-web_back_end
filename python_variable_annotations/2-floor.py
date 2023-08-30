04d4476218a44ff72c08c6354ef5b46ae1ac4107
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-30 11:09:05 -0700

    function floor ( type-annotated )

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that contains a function that transforms
+a decimal into an integer"""
+
+
+def floor(n: float) -> int:
+    """Function that returns an integer"""
+    return int(n)
