f8b535d5009199ae76d487820620a94906d7d6fe
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-30 11:25:40 -0700

    function sum_list ( type-annotated )

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that contains a function"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Function that adds a list"""
+    return sum(input_list)
