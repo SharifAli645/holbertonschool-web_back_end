5e56324d092a617b2911c1555562b5a9e38a54ea
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-30 20:37:02 -0700

    tyoe annotation

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Module that contains a function async"""
 import asyncio
-from typing import Generator
+from typing import List
 import random
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Function generator async"""
     result = []
     async for i in async_generator():
