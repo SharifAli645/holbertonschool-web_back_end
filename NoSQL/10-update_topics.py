e070dee8fc4d1b46a1d1d872c298a0a00f3bff2b
SharifAli645 <pandemiaonirica@gmail.com>
2023-09-06 08:29:20 -0700

    python function (mongodb)

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function that inserts a document in a
+collection"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Function that updates a document"""
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
