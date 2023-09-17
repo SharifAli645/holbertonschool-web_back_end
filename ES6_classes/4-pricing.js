269fedcac046032cbfedbe1258498c1f3ba39002
SharifAli645 <pandemiaonirica@gmail.com>
2023-09-17 21:19:27 +0000

    js class

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,45 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof currency !== 'object') {
+      throw new TypeError('Currency must be an object');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+  
+  get amount() {
+    return this._amount;
+  }
+  
+  get currency() {
+    return this._currency;
+  }
+  
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+  
+  set currency(valu) {
+    if (typeof valu !== 'object') {
+      throw new TypeError('Currency must be a number');
+    }
+    this._currency = valu;
+  }
+  
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`
+  }
+  
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+  
+}
