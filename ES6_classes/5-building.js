a9aa3071ac9d996fbbc7e4fedbdf2c967aba6641
SharifAli645 <120497885+SharifAli645@users.noreply.github.com>
2023-09-17 17:15:37 -0500

    Update 5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,14 +1,17 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    if (this.constructor !== 'Building') {
+      this.evacuationWarningMessage();
+    }
   }
 
   get sqft() {
     return this._sqft;
   }
+
   evacuationWarningMessage() {
-    if (this.constructor !== 'Building') {
-      throw new Error("Class extending Building must override evacuationWarningMessage");
+    throw new Error("Class extending Building must override evacuationWarningMessage");
     }
   }
 }
