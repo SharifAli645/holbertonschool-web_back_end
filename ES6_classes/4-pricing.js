6bc42dc780ab5d3d5c7b826b6d61fdfe3b80cd5b
SharifAli645 <120497885+SharifAli645@users.noreply.github.com>
2023-09-17 16:37:56 -0500

    fit: eslint

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,3 +1,4 @@
+/*eslint-disable-next-line*/
 import Currency from './3-currency';
 
 export default class Pricing {
@@ -35,7 +36,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
