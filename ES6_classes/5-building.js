6a6286984703e27b0c587e21530ae8366593106f
SharifAli645 <120497885+SharifAli645@users.noreply.github.com>
2023-09-17 17:35:10 -0500

    Update 5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
@@ -10,7 +11,6 @@ export default class Building {
     return this._sqft;
   }
   
-  // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
