9e56ee949c6861e481154b7524fa542013d8a7a8
SharifAli645 <pandemiaonirica@gmail.com>
2023-09-17 21:33:34 +0000

    JS class

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -11,35 +11,33 @@ export default class Pricing {
     this._amount = amount;
     this._currency = currency;
   }
-  
+
   get amount() {
     return this._amount;
   }
-  
+
   get currency() {
     return this._currency;
   }
-  
+
   set amount(valu) {
     if (typeof valu !== 'number') {
       throw new TypeError('Amount must be a number');
     }
     this._amount = valu;
   }
-  
+
   set currency(valu) {
     if (typeof valu !== 'object') {
       throw new TypeError('Currency must be a number');
     }
     this._currency = valu;
   }
-  
+
   displayFullPrice() {
     return `${this._amount} ${this._currency.name} (${this._currency.code})`
   }
-  
+
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
-  
-}
