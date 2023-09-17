c4236cd65716bcc08ccdfa93ccad62da730b1033
SharifAli645 <120497885+SharifAli645@users.noreply.github.com>
2023-09-17 14:44:10 -0500

    fit: eslint

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,5 +1,5 @@
-import ClassRoom from './0-classroom.js';
-
+import ClassRoom from './0-classroom';
+/* eslint-disable-next-line */
 function initializeRooms() {
   const array = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
   return array;
