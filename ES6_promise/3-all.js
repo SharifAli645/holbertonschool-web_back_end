3881d5a225c7ace2b5a7f4ba42860a8e6817c1fc
SharifAli645 <pandemiaonirica@gmail.com>
2023-10-09 11:22:59 -0500

    sintaxis

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,13 +1,17 @@
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
+function handleProfileSignup () {
   let body = '';
-  uploadPhoto().then((success) => {
-    body = success.body;
-  });
-  createUser().then((success) => {
-    console.log(`${body} ${success.firstName} ${success.lastName}`);
-  });
+  uploadPhoto()
+    .then((success) => {
+      body = success.body;
+    })
+    .catch(() => console.log('Signup system offline'));
+  createUser()
+    .then((success) => {
+      console.log(`${body} ${success.firstName} ${success.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
