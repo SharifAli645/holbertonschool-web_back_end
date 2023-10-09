ffbe058bf7aaf241855481f65597ea6ca53fe29f
SharifAli645 <pandemiaonirica@gmail.com>
2023-10-09 14:37:56 -0500

    JS function

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,5 @@
 export default function getListStudentIds(arr) {
-  console.log(typeof arr);
-  if (typeof arr === 'object') {
+  if (arr instanceof Array) {
     return arr.map((student) => student.id);
   }
   return [];
