c54d1efed45312cf986e11f80e0019157474892c
SharifAli645 <pandemiaonirica@gmail.com>
2023-10-09 10:08:21 -0500

    fix: sintaxis

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,10 @@
 export default function getResponseFromAPI () {
   return new Promise((resolve, reject) => {
-    // Promise
+    const success = true;
+    if (success) {
+      resolve('');
+    } else {
+      reject(error);
+    }
   });
 }
