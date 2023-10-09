1d3058506a2cc738cb56839e9331496eca5490e9
SharifAli645 <pandemiaonirica@gmail.com>
2023-10-09 14:20:55 -0500

    function named getListStudents that returns an array of objects

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,19 @@
 export default function getListStudents() {
-  const array = [
+  return [
     {
       id: 1,
       firstName: 'Guillaume',
-      location: 'San Francisco'
+      location: 'San Francisco',
     },
     {
       id: 2,
       firstName: 'James',
-      location: 'Columbia'
+      location: 'Columbia',
     },
     {
       id: 3,
       firstName: 'Serena',
-      location: 'San Francisco'
+      location: 'San Francisco',
     }
   ];
-  return array;
 }
