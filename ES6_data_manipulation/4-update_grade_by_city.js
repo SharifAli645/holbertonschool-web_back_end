abf5ba389ec666147f7b2617422525a6770310b1
SharifAli645 <pandemiaonirica@gmail.com>
2023-10-09 18:57:09 -0500

    JS function

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,11 @@
+/* eslint-disable */
 function updateStudentGradeByCity(students, city, array) {
   return students
     .filter(({ location }) => location === city)
     .map((student) => {
       const gr = array.filter((grade) => grade.studentId === student.id);
       if (gr.length !== 0) student.grade = gr[0].grade;
-      else student.grade = 'N/A';
+      else student.grade = "N/A";
       return student;
     });
 }
