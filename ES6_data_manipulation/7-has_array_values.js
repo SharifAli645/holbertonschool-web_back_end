5adde86be565cdeaeddea3decfaf5ee5574d901c
SharifAli645 <pandemiaonirica@gmail.com>
2023-10-09 19:42:50 -0500

    js function

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,5 @@
 function hasValuesFromArray(set, array) {
-  const n = array.map(numero => set.has(numero));
+  const n = array.map((numero) => set.has(numero));
   const seti = new Set(n);
   return !seti.has(false);
 }
