c5df3ed54096f2a2e7c4dcf24f20d5e38ada643a
SharifAli645 <pandemiaonirica@gmail.com>
2023-10-09 21:10:25 -0500

    js function

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,10 @@
 export default function cleanSet(set, string) {
   const list = [];
-  if (string && typeof string === "string") {
+  if (string && typeof string === 'string') {
     const arr = Array.from(set);
     arr.forEach((str) => {
-      if (str && str.startsWith(string))
-        list.push(str.slice(string.length));
+      if (str && str.startsWith(string)) { list.push(str.slice(string.length)); }
     });
   }
-  return list.join("-");
+  return list.join('-');
 }
